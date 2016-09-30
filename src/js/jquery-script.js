21a8bb2cf7c6eba34258d90e8c4926a3b67bdacb
yanxiaodi <929213769@qq.com>
2016-09-30 18:02:57 +0800

    backup

--- src/js/jquery-script.js
+++ src/js/jquery-script.js
@@ -1,33 +1,35 @@
 $(document).ready(function(){
 
-    // $.ajax({
-    //     type: "POST",
-    //     url: url,
-    //     data: data,
-    //     success: success,
-    //     dataType: dataType
+    // 周边房源　按钮
+    // $("＃search-housing").click(function(){
+    //     $.post("demo.html",{
+    //             lng : mPoint.lng,
+    //             lat : mPoint.lat
+    //     },function(data,status){
+    //         for (var i = 0; i < data.length; i++) {
+    //             var Arr = new Object();
+    //             Arr = data[i];
+    //             // 拼接属性文字内容
+    //             var text = "￥" + Arr.priceBeginning + Arr.beginUnit +  "起",
+    //                 mouseoverTxt = Arr.name + " " + Arr.resourceAmount + "套" ;
+    //             var myCompOverlay = new ComplexCustomOverlay(
+    //                 new BMap.Point(Arr.longitude,Arr.latitude),text,mouseoverTxt
+    //             );
+    //             map.addOverlay(myCompOverlay);
+    //         }
+    //     });
+    //
     // });
 
-    // 周边房源　按钮
-    $("＃search-housing").click(function(){
-
-
-        $.post("demo.html",function(data,status){
-            // Ajax添加单个自定义覆盖物
-            function addBuilding(ObjGroup){
-                for (var i = 0; i < ObjGroup.length; i++) {
-                    var Arr = new Object();
-                    Arr = ObjGroup[i];
-                    // 拼接属性文字内容
-                    var text = "￥" + Arr.priceBeginning + Arr.beginUnit +  "起",
-                        mouseoverTxt = Arr.name + " " + Arr.resourceAmount + "套" ;
-                    var myCompOverlay = new ComplexCustomOverlay(
-                        new BMap.Point(Arr.longitude,Arr.latitude),text,mouseoverTxt
-                    );
-                    map.addOverlay(myCompOverlay);
-                }
-            };
-        });
-
+    $.ajax({
+        type: "POST",
+        url: url,
+        data: mPoint,
+        success: function(backDate){
+            $( "＃search-housing" ).each(function ( i, domEle) {
+                
+            });
+        },
+        dataType: dataType
     });
 });
