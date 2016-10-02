22c083d623ea2d0ce8f023da0186c69323500d97
yanxiaodi <929213769@qq.com>
2016-10-03 00:33:29 +0800

    整理分拆JS文件，方便维护；未进行美化

--- src/js/var.js
+++ src/js/var.js
@@ -1,21 +1,21 @@
-// /// 圆形检索填充////
-// var circle = new BMap.Circle(mPoint,1000,{
-//     fillColor:"#FF2725",						// 填充色
-//     strokeWeight: 2,							// 画笔宽度
-//     strokeColor: "#FF2725",
-//     fillOpacity: 0.07, 							// 填充透明度
-//     strokeOpacity: 1							// 画笔透明度
-// });
-// // 控件声明
-// searchHousing = document.getElementById("search-housing");
-// searchTraffic = document.getElementById("search-traffic");
-// searchSnack = document.getElementById("search-snack");
-// searchRestaurant = document.getElementById("search-restaurant");
-// searchBank = document.getElementById("search-bank");
-// searchHotel = document.getElementById("search-hotel");
-// // list表单"在地图上显示"按钮
-// var Btntraffic = document.getElementById("OutputList-traffic");
-// var Btnsnack = document.getElementById("OutputList-snack");
-// var Btnrestaurant = document.getElementById("OutputList-restaurant");
-// var Btnbank = document.getElementById("OutputList-bank");
-// var Btnhotel = document.getElementById("OutputList-hotel");
+/// 圆形检索填充
+var circle = new BMap.Circle(mPoint,1000,{
+    fillColor:"#FF2725",						// 填充色
+    strokeWeight: 2,							// 画笔宽度
+    strokeColor: "#FF2725",
+    fillOpacity: 0.07, 							// 填充透明度
+    strokeOpacity: 1							// 画笔透明度
+});
+
+var local =  new BMap.LocalSearch(map, {
+    renderOptions: {
+        map: map,
+        autoViewport: false
+    }
+});
+
+var Btntraffic = document.getElementById("OutputList-traffic");
+var Btnsnack = document.getElementById("OutputList-snack");
+var Btnrestaurant = document.getElementById("OutputList-restaurant");
+var Btnbank = document.getElementById("OutputList-bank");
+var Btnhotel = document.getElementById("OutputList-hotel");
