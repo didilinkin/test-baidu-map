c1c1765fc2f28e33ce50daa8bbdda1b9c65bddd2
yanxiaodi <929213769@qq.com>
2016-10-08 11:17:42 +0800

    修改list数值获取方法，修改检索范围

--- src/js/call.js
+++ src/js/call.js
@@ -2,23 +2,50 @@
  *调用——页面加载中按顺序做的内容
  */
 var map = new BMap.Map("allmap", {enableMapClick:false});   // 创建Map实例(关闭底图可点功能)
-var mPoint = new BMap.Point(120.405475,36.086421);    // 标点位置(华润大厦)
+var mPoint = new BMap.Point(120.384459,36.071709);    // 标点位置(华润大厦)
 var myIcon = new BMap.Icon("./images/pt.png", new BMap.Size(65,80));    // 自定义标注样式
 var customMarker = new BMap.Marker(mPoint,{icon:myIcon});  // 创建自定义标注(将样式加入)
     customMarker.setAnimation(BMAP_ANIMATION_BOUNCE);     // 设置自定义标注 跳动
 map.centerAndZoom(mPoint,15);   // 创建地图中心点,层级15级（并不显示标记）
 map.disableDragging();  //禁止拖拽
 // 添加 缩放 与 平移控件
-map.addControl(new BMap.NavigationControl(
- {
-     anchor: BMAP_ANCHOR_TOP_RIGHT,
-     type: BMAP_NAVIGATION_CONTROL_SMALL
- }));
+map.addControl(new BMap.NavigationControl({
+    anchor: BMAP_ANCHOR_TOP_RIGHT,
+    type: BMAP_NAVIGATION_CONTROL_SMALL
+}));
+// 自定义控件(右上角 筛选)
+var controlLiId = ["周边房源","公交","快餐店","餐厅","银行","大酒店"];      // 检索关键词
+function ulListControl(){
+ this.defaultAnchor = BMAP_ANCHOR_TOP_LEFT;      // 默认停靠位置和偏移量
+ this.defaultOffset = new BMap.Size(10, 10);
+}
+ulListControl.prototype = new BMap.Control();
+// 自定义控件必须实现自己的initialize（初始化）方法,并且将控件的DOM元素返回
+ulListControl.prototype.initialize = function(map){
+    var ul = document.createElement("ul");      // 创建一个DOM元素
+    ul.id="ulList";
+    var li = null;      // 创建一个节点
+    var aText = ["周边房源", "交通","快餐","餐厅","银行","酒店"];     // 将li内容创建为数组
+    var aIdName = ["housing", "traffic","snack","restaurant","bank","hotel"];
+    // 循环li标签
+    for (var i=0; i < controlLiId.length; i++){
+        li = document.createElement("li");
+        a = document.createElement("a");
+        li.setAttribute("onclick","controlLiOnclick("+ "\"" + controlLiId[i] + "\"" + "," + i +")");     // 给li控件添加点击事件
+        a.setAttribute("herf","#");     // 给a标签添加属性
+        a.appendChild(document.createTextNode(aText[i]));
+        a.id=("search-" + aIdName[i]);
+        ul.appendChild(li);
+        li.appendChild(a);
+    }
+    map.getContainer().appendChild(ul);     // 添加DOM元素到地图中
+    return ul;      // 将DOM元素返回
+}
+var myUlListControl = new ulListControl();
+map.addControl(myUlListControl);    // 将控件重新添加到地图中
 //两秒后开启拖拽
 setTimeout(function(){
     map.enableDragging();
 }, 3500);
-// 输出list列表内容
-reloadList();
-// 将自定义标注、自定义控件添加到地图中
-defaultCallLoad()
+reloadList();       // 输出list列表内容
+defaultCallLoad();      // 将自定义标注、自定义控件添加到地图中
--- src/js/statement.js
+++ src/js/statement.js
@@ -6,6 +6,9 @@ var Btntraffic = document.getElementById("OutputList-traffic"),
     Btnrestaurant = document.getElementById("OutputList-restaurant"),
     Btnbank = document.getElementById("OutputList-bank"),
     Btnhotel = document.getElementById("OutputList-hotel");
+var infoWindowMarker = new BMap.Marker(mPoint);
+var oneMarker = new BMap.Marker(mPoint);
+var listBtn = [Btntraffic,Btnsnack,Btnrestaurant,Btnbank,Btnhotel]      // list列表Btn名称
 // 圆形检索填充
 var circle = new BMap.Circle(mPoint,1000,{
     fillColor:"#FF2725",						// 填充色
@@ -24,10 +27,10 @@ var local =  new BMap.LocalSearch(map, {
 function reloadList() {
     var allListId = [
         // 检索标题,列表ulID位置,列表pID输出位置
-        {title: "公交站",ulId: "List-traffic",pId: "Num-traffic"},
+        {title: "公交",ulId: "List-traffic",pId: "Num-traffic"},
         {title: "快餐店",ulId: "List-snack",pId: "Num-snack"},
-        {title: "西餐厅",ulId: "List-restaurant",pId: "Num-restaurant"},
-        {title: "银行,支行",ulId: "List-bank",pId: "Num-bank"},
+        {title: "餐厅",ulId: "List-restaurant",pId: "Num-restaurant"},
+        {title: "银行",ulId: "List-bank",pId: "Num-bank"},
         {title: "大酒店",ulId: "List-hotel",pId: "Num-hotel"}
     ];
     for (var i = 0; i < 5; i++) {
@@ -58,7 +61,7 @@ function reloadList() {
                         listUl.appendChild(li);
     				}
                     var PNum = document.getElementById(listP);      // 检索数值
-                    var num = results.getNumPois();
+                    var num = results.getCurrentNumPois();   // getNumPois()全部结果数量
                     var numnode = document.createTextNode(num);
                     PNum.appendChild(numnode);
                     listUl.setAttribute("onmouseout","liOnmouseout()");     // listUl添加鼠标移出事件
@@ -66,7 +69,7 @@ function reloadList() {
             }
         };
         var local = new BMap.LocalSearch(mPoint,options);
-        local.searchNearby(listTile, mPoint,600);
+        local.searchNearby(listTile, mPoint,1000);
     }
 }
 // 默认调用
@@ -79,7 +82,6 @@ function clearMapAgainMarker(){
     map.clearOverlays();    // 清理地图上面所有点
     defaultCallLoad();
 }
-var oneMarker = new BMap.Marker(mPoint);    // 定义全局变量
 // 单独标记(只标记不输出)
 function addMarker(PointLng,PointLat) {
     clearMapAgainMarker();          // 清空地图,重新打上中心自定义标记
@@ -103,8 +105,6 @@ function liOnclick(liLng,liLat,text,liAddress){
     addInfoWindow(liLng,liLat,text,liAddress);
     infoWindowMarker.setAnimation(BMAP_ANIMATION_BOUNCE);     // 设置自定义标注 跳动
 }
-//
-var infoWindowMarker = new BMap.Marker(mPoint);
 // 添加信息窗口实例
 function addInfoWindow(liLng,liLat,text,liAddress){
     var opts = {
@@ -130,38 +130,6 @@ function addInfoWindow(liLng,liLat,text,liAddress){
     }
     return infoWindowMarker;
 }
-// 自定义控件(右上角 筛选)
-var controlLiId = ["周边房源","公交站","快餐店","西餐厅","银行,支行","大酒店"];      // 检索关键词
-function ulListControl(){
-    this.defaultAnchor = BMAP_ANCHOR_TOP_LEFT;      // 默认停靠位置和偏移量
-    this.defaultOffset = new BMap.Size(10, 10);
-}
-ulListControl.prototype = new BMap.Control();
-// 自定义控件必须实现自己的initialize（初始化）方法,并且将控件的DOM元素返回
-ulListControl.prototype.initialize = function(map){
-    var ul = document.createElement("ul");      // 创建一个DOM元素
-    ul.id="ulList";
-    var li = null;      // 创建一个节点
-    var aText = ["周边房源", "交通","快餐","餐厅","银行","酒店"];     // 将li内容创建为数组
-    var aIdName = ["housing", "traffic","snack","restaurant","bank","hotel"];
-    // 循环li标签
-    for (var i=0; i < controlLiId.length; i++){
-        li = document.createElement("li");
-        a = document.createElement("a");
-        li.setAttribute("onclick","controlLiOnclick("+ "\"" + controlLiId[i] + "\"" + "," + i +")");     // 给li控件添加点击事件
-        a.setAttribute("herf","#");     // 给a标签添加属性
-        a.appendChild(document.createTextNode(
-            aText[i]
-        ));
-        a.id=("search-" + aIdName[i]);
-        ul.appendChild(li);
-        li.appendChild(a);
-    }
-    map.getContainer().appendChild(ul);     // 添加DOM元素到地图中
-    return ul;      // 将DOM元素返回
-}
-var myUlListControl = new ulListControl();
-map.addControl(myUlListControl);    // 将控件重新添加到地图中
 // 控件点击搜索事件（参数:检索词）
 function controlLiOnclick(controlSearchLiId,index){
     // 如果 参数不等于 "周边房源"
@@ -186,44 +154,39 @@ function controlLiOnclick(controlSearchLiId,index){
     		}
     	};
     	var local = new BMap.LocalSearch(mPoint, options);
-        local.searchNearby(controlSearchLiId, mPoint,600);    // 检索词
+        local.searchNearby(controlSearchLiId, mPoint,1000);    // 检索词
         map.addOverlay(circle);
         btnClearClass();
-        listBtn[index - 1].setAttribute("class","active");    // list列表名称
-        // console.log("1");
+        listBtn[index-1].setAttribute("class","active");        // list列表名称
     }
 }
-
-
-
-
-
 Btntraffic.onclick = function(){
     controlLiOnclick(controlLiId[1]);
     btnClearClass();
-    listBtn[0].setAttribute("class","active");
-};
+    listBtn[0].setAttribute("class","active");};
 Btnsnack.onclick = function(){
-    controlLiOnclick(controlLiId[2]);       // 检索关键词（关键词）
+    controlLiOnclick(controlLiId[2]);
     btnClearClass();
-    listBtn[1].setAttribute("class","active");
-};
+    listBtn[1].setAttribute("class","active");};
 Btnrestaurant.onclick = function(){
     controlLiOnclick(controlLiId[3]);
     btnClearClass();
     listBtn[2].setAttribute("class","active");
 };
-Btnbank.onclick = function(){
-    controlLiOnclick(controlLiId[4]);
-    btnClearClass();
-    listBtn[3].setAttribute("class","active");
-};
+// Btnbank.onclick = function(){
+//
+// };
+function BtnSearchBank() {
+    controlLiOnclick(controlLiId[4],0);
+    // btnClearClass();
+    var Btnbank = document.getElementById("OutputList-bank");
+    // Btnbank.setAttribute("class","active");
+    console.log(Btnbank);
+}
 Btnhotel.onclick = function(){
     controlLiOnclick(controlLiId[5]);
     btnClearClass();
-    listBtn[4].setAttribute("class","active");
-};
-var listBtn = [Btntraffic,Btnsnack,Btnrestaurant,Btnbank,Btnhotel]      // list列表Btn名称
+    listBtn[4].setAttribute("class","active");};
 // 清理list列表class样式
 function btnClearClass(){
     for (var i = 0; i < listBtn.length; i++) {
